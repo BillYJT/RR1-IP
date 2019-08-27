ee1fdc828d241dd7e8b573d0abddf7c50b79dcac
BillYJT <36845351+BillYJT@users.noreply.github.com>
2019-08-27 23:03:50 +1000

    Update temp_publisher.ino
    Update temp publisher

--- temp_publisher.ino
+++ temp_publisher.ino
@@ -1,4 +1,3 @@
-
 #include <ros.h>
 #include <std_msgs/String.h>
 #include <sensor_msgs/LaserScan.h>
@@ -25,36 +24,32 @@ void setup(){
   temp_msg.header.frame_id =  frameid;
   temp_msg.angle_min = 0.0;
   temp_msg.angle_max = 6.28;  
-  temp_msg.angle_increment = 3.14; 
+  temp_msg.angle_increment = 6.28/40; 
   temp_msg.range_min = 0.03;  
   temp_msg.range_max = 7; 
   temp_msg.scan_time = 0.2;
-  //temp_msg.ranges_length = 80;
+  temp_msg.ranges_length = 40;
+  //temp_msg.intensities_length = 2;
 }
 
-int num_readings = 2;//(temp_msg.angle_max - temp_msg.angle_min)/temp_msg.angle_increment;
+int num_readings = 40;
 
 void loop(){
   int dir = dir_gen(analog_pin);
-  float range[num_readings];
+  float range[num_readings] = {0};
+  int angle_start = (dir-1)*num_readings/4;
+  int angle_end = dir*num_readings/4;
 
-  if (1){   
-    for(unsigned int i = 0; i < num_readings;  i++){
-     
-      temp_msg.ranges[i] = 6.0;
-  
-      nh.loginfo("aa");
-    }
+  if ( (millis()-timer) > 200){
     
-  }  
-  //Need to add cases for other directions
-
-  //for(unsigned int i = 0; i < num_readings; i++){
-  //  temp_msg.ranges[i] =  range[i];
-  //}
-  
-  temp_msg.header.stamp = nh.now();
-  pub_temp.publish(&temp_msg);
-  //timer =  millis();
+    for(unsigned int i = angle_start; i < angle_end;  i++){
+        range[i] = 6.0;
+    }
+     
+    temp_msg.ranges = range;
+    temp_msg.header.stamp = nh.now();
+    pub_temp.publish(&temp_msg);
+    timer =  millis();
+  }
   nh.spinOnce();
 }
