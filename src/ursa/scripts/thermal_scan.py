bea3c5e0eb8f42b243840548e173b6e356dfa49e
BillYJT <36845351+BillYJT@users.noreply.github.com>
2019-10-11 18:34:55 +1100

    small fix on thermal

--- src/ursa/scripts/thermal_scan.py
+++ src/ursa/scripts/thermal_scan.py
@@ -6,7 +6,7 @@ import sensor_msgs.msg
 import numpy as np
 
 hot_scan = sensor_msgs.msg.LaserScan()
-hot_scan.angle_min = -27925
+hot_scan.angle_min = -0.27925
 hot_scan.angle_max = 0.27925
 hot_scan.angle_increment = 0.01745
 hot_scan.range_min = 0.03
@@ -24,10 +24,10 @@ def imageCB(data):
         for j in range(10,20): #row
             tmp[i] = tmp[i] + data.data[32*j+i]
 
-    tmp_mask = (tmp>200)*1
+    tmp_mask = (tmp>300)*1
     #print(tmp)
     #print(tmp_mask)
-    ranges = np.multiply(np.divide(2000,tmp) + 1, tmp_mask) #Publish points between 1 and 2m
+    ranges = np.multiply(np.divide(300,tmp) + 1, tmp_mask) #Publish points between 1 and 2m
     hot_scan.ranges = ranges    
     hot_scan.header.stamp = rospy.Time.now()
     pub.publish(hot_scan)
